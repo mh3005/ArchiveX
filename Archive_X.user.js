6a8381bcfc91229a6ce07f5527383c757dfbcf43
MH <fyseaking9002@aim.com>
2015-11-12 12:24:00 -0800

    Adding download URL

--- Archive_X.user.js
+++ Archive_X.user.js
@@ -4,6 +4,7 @@
 // @description Image expander for fgts.jp
 // @include     http://fgts.jp/*
 // @version     0.1.0
+// @downloadURL https://raw.githubusercontent.com/mh3005/ArchiveX/master/Archive_X.user.js
 // @grant       none
 // ==/UserScript==
 
