202f4d25e6943e499404d88f00ba52ed87ccf518
MH <fyseaking9002@aim.com>
2015-11-12 12:18:54 -0800

    Adding source placeholder and gitignore

---
+++ Archive_X.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name        Archive X
+// @namespace   archive_x
+// @description Image expander for fgts.jp
+// @include     http://fgts.jp/*
+// @version     0.1.0
+// @grant       none
+// ==/UserScript==
+
+(function(){}
+  "use strict";
+ 
+ // Define Helper Functions
+ 
+ // Main function
+ function main() {
+  
+  }
+ 
+  main();
+)()
